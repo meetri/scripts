8dec1a64dd32b06457b81355c0796617c6babf4e
Demetrius Bell <meetri@gmail.com>
2024-11-03 23:06:49 +0000

    gluetun docker vpn

---
+++ docker/vpn/speedtest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# https://askubuntu.com/questions/104755/how-to-check-internet-speed-via-terminal
+## run this inside qbitorrent to see speed of vpn
+curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
